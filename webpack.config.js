59391ac629c90a9c4625aafd262a90512491f10d
Jaroslav Holeček <holecek.jaroslav@email.cz>
2024-03-29 15:04:43 +0100

    bundle webpack

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+// webpack.config.js
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js', // Entry point of your application
+    output: {
+        path: path.resolve(__dirname, 'dist'), // Output directory
+        filename: 'matchdraw_bundle.js' // Output file name
+    },
+    mode: 'development' // Set mode to development or production
+};
