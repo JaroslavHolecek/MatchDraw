d1c599687cb28fe241dc0708aea5528c6d857ae2
Jaroslav Holeček <holecek.jaroslav@email.cz>
2024-03-29 16:58:57 +0100

    browserify

--- dist/bundle.js
+++ dist/bundle.js
@@ -1,4 +1,4 @@
-(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.MatchDraw = f()}})(function(){var define,module,exports;return (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 const { NotOverridenFunction, NotSupportedAttributeValue } = require('../Core/MD_Errors');
 
 /**
@@ -1473,4 +1473,5 @@ const {Participant_Radon, Tournament_Swiss_Radon} = require('./Competition/MD_Sw
 console.log("See examples in showroom directory for usage example.");
 
 module.exports = { RESULT_TEMPLATES, Participant_Radon, Tournament_Swiss_Radon };
-},{"./Competition/MD_Options":2,"./Competition/MD_Swiss_Radon":3}]},{},[9]);
+},{"./Competition/MD_Options":2,"./Competition/MD_Swiss_Radon":3}]},{},[9])(9)
+});
